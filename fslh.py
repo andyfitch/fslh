0cffa393931e291536cb7e26329eb1681260fb7b
Andy Fitch <andy@parall.ax>
2014-07-10 14:32:54 +0100

    Delimiter

--- fslh.py
+++ fslh.py
@@ -5,7 +5,7 @@ import math
 class FontSizeLineHeightCommand(sublime_plugin.TextCommand):
 	def run(self, view):
 		for region in self.view.sel():
-			selection = self.view.substr(region).split(',')
+			selection = self.view.substr(region).split('/')
 			fontem = (int(selection[1]) / int(selection[0]))
 			lineem = (int(selection[2]) / int(selection[1]))
 			output = 'font-size: ' + str(fontem) + 'em;\nline-height: ' + str(lineem) + 'em;'
